cdcef5d52f53b974aaba19b970e2cdb8cc25f89c
Bjornskjald <github@bjorn.ml>
2018-10-20 12:16:57 +0200

    initial commit

--- Miscord/ViewController.swift
+++ Miscord/ViewController.swift
@@ -8,12 +8,55 @@
 
 import Cocoa
 
+func shell(launchPath: String) -> Pipe {
+    let task = Process()
+    task.launchPath = launchPath
+    
+    let pipe = Pipe()
+    task.standardOutput = pipe
+    task.standardError = pipe
+    task.launch()
+    
+    return pipe
+}
+
+func getContaineredPath(path: String) -> String {
+    return FileManager.default.homeDirectoryForCurrentUser.path + path
+}
+
 class ViewController: NSViewController {
+    
+    @IBOutlet var textView: NSTextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let path = Bundle.main.path(forResource: "miscord-mac", ofType: "bin")
+        if (path == nil) {
+            textView.string = "Binary not found."
+            return
+        }
+        let output = shell(launchPath: path!)
+        let outHandle = output.fileHandleForReading
+        outHandle.waitForDataInBackgroundAndNotify()
+        
+        var progressObserver : NSObjectProtocol!
+        progressObserver = NotificationCenter.default.addObserver(
+            forName: NSNotification.Name.NSFileHandleDataAvailable,
+            object: outHandle, queue: nil)
+        {
+            notification -> Void in
+            let data = outHandle.availableData
+            
+            if data.count > 0 {
+                if let str = String(data: data, encoding: String.Encoding.utf8) {
+                    self.textView.string += str
+                }
+                outHandle.waitForDataInBackgroundAndNotify()
+            } else {
+                // That means we've reached the end of the input.
+                NotificationCenter.default.removeObserver(progressObserver)
+            }
+        }
     }
 
     override var representedObject: Any? {
@@ -21,7 +64,15 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
-
+    
+    @IBAction func openConfigLocationClicked(_ sender: Any) {
+        let alert = NSAlert.init()
+        alert.messageText = "Hello world"
+        alert.informativeText = getContaineredPath(path: "/Library/Application Support/Miscord/")
+        alert.addButton(withTitle: "OK")
+        alert.addButton(withTitle: "Cancel")
+        alert.runModal()
+        NSWorkspace.shared.openFile(getContaineredPath(path: "/Library/Application Support/Miscord/"))
+    }
 }
 
