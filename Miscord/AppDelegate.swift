566fd48d8d96fcb2b6e2c6d51ec74101ebc6c3e6
Bjornskjald <github@bjorn.ml>
2018-10-20 11:07:02 +0200

    Initial Commit

---
+++ Miscord/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Miscord
+//
+//  Created by Bjornskjald on 20.10.2018.
+//  Copyright © 2018 Miscord. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Miscord/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Miscord
+//
+//  Created by Bjornskjald on 20.10.2018.
+//  Copyright © 2018 Miscord. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
